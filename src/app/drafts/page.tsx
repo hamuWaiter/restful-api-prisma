2fd379beb295dfd557c942987806efefe95f7a2b
hamuWaiter <54763552+hamuWaiter@users.noreply.github.com>
2024-06-06 15:57:32 +0800

    初始化

---
+++ src/app/drafts/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Post from '../../components/Post'
+import prisma from '../../lib/prisma'
+import styles from '../../styles/Drafts.module.css'
+
+export default async function Drafts() {
+  const drafts = await prisma.post.findMany({
+    where: { published: false },
+    include: { author: true },
+  })
+
+  console.log('drafts: ', drafts)
+
+  return (
+    <>
+      <div>
+        <h1>Drafts</h1>
+        <main>
+          {drafts.map((post) => (
+            <div key={post.id} className={styles.post}>
+              <Post post={post} />
+            </div>
+          ))}
+        </main>
+      </div>
+    </>
+  )
+}
